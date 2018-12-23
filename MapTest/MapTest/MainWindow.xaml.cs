141892a7f73eef1c1bef4b1d679185868ad8b8cd
natanno4 <natanno4@gmail.com>
2018-12-23 21:32:20 +0200

    add map test

---
+++ MapTest/MapTest/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Map map = new Map();
+
+        public string DisplayedImage
+        {
+            get { return map.MapImagePath; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+       public void OnMapClick(object sender, MouseEventArgs e)
+        {
+            //calculate map statring point
+            double mapminHeight = worldMap.Margin.Top;
+            double mapminWidth = worldMap.Margin.Left;
+
+            //calculate position x and y , for coordinates calculation
+            Point p = e.GetPosition(this);
+            //get map difference to default map.
+            double[] dif = map.getMapSizeDiffernce();
+            double x = (p.X - mapminWidth)/dif[0];
+            double y = (p.Y - mapminHeight)/dif[1];
+            map.fromPixelToCoordinates(x, y);
+            double lon = map.CurrentLongitude;
+            double la = map.CurrentLatitude;
+
+            //draw circle
+            Ellipse el = new Ellipse();
+            el.Width = 5;
+            el.Height = 5;
+            el.Fill = new SolidColorBrush(Colors.Red);
+            double left = p.X - (el.Width / 2);
+            double top = p.Y - (el.Height / 2);
+            el.Margin = new Thickness(left, top, 0, 0);
+            el.VerticalAlignment = VerticalAlignment.Top;
+            el.HorizontalAlignment = HorizontalAlignment.Left;
+            girdMap.Children.Add(el);         
+         }
+
+    }
+}
---
+++ MapTest/MapTest/Map.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace MapTest
+{
+    class Map
+    {
+        private const double mapWidthdef = 360;
+        private const double mapHeightdef = 180;
+        private int mapWidth;
+        private int mapHeight;
+        private string mapImagePath;
+        public Map()
+        {
+            string settings = ConfigurationManager.AppSettings["mapSettings"];
+            string[] settingsArray = settings.Split(';');
+            mapImagePath = settingsArray[0];
+            Int32.TryParse(settingsArray[2], out mapHeight);
+            Int32.TryParse(settingsArray[1], out mapWidth);
+            CurrentLatitude = 0;
+            CurrentLongitude = 0;
+        }
+
+        public int MapHeight
+        {
+            get
+            {
+                return this.mapHeight;
+            }
+        }
+
+        public int MapWidth
+        {
+            get
+            {
+                return this.mapWidth;
+            }
+        }
+
+        public string MapImagePath
+        {
+            get
+            {
+                return this.mapImagePath;
+            }
+        }
+
+        public double CurrentLatitude
+        {
+            get; set;
+        }
+
+        public double CurrentLongitude
+        {
+            get; set;
+        }
+
+        public void fromPixelToCoordinates(double x, double y)
+        {
+            CurrentLongitude = x - 180;
+            CurrentLatitude = 90 - y;
+        }
+
+        public double[] getMapSizeDiffernce()
+        {
+            double[] dif = {MapWidth / mapWidthdef, MapHeight / mapHeightdef};
+            return dif;
+        }
+
+    }
+}
