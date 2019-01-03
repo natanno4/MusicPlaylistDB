fe8c6b16e5a355a2d074ab39d4fe2d8bb54923ab
niraz888 <niraz888@gmail.com>
2019-01-03 15:27:28 +0200

    copy query to code

--- MusicPlayList/MusicPlayList/Model/LocationMapChooserModel.cs
+++ MusicPlayList/MusicPlayList/Model/LocationMapChooserModel.cs
@@ -37,14 +37,22 @@ namespace MusicPlayList.Model
         {
             // need to be changed later, maybe in sunday
             StringBuilder subQuery = null;
-            subQuery.Append("SELECT area.location_name, area.location_id FROM musicareaplaylist.area ");
+            subQuery.Append("Select LocationId, location_name, count(location_name) FROM ");
+            subQuery.Append("(SELECT area.LocationId,area.location_name FROM music_area_playlist.area ");
             subQuery.Append("WHERE area.latitude != 0 AND area.longitude != 0 ");
             subQuery.Append("GROUP BY area.location_name ");
-            subQuery.Append("ORDER BY (6371 * acos( cos( radians(area.latitude) ) * cos( radians("+ area.Latitude.ToString()+ ")) * cos( radians("+ area.Longtitude.ToString()+") - radians(area.longitude) ) ");
+            subQuery.Append("order by (6371 * acos( cos( radians(area.latitude) ) * cos( radians(" + area.Latitude.ToString() + ")) ");
+            subQuery.Append("* cos( radians("+Area.Longtitude.ToString()+") - radians(area.longitude) ) + sin( radians(area.latitude) ) * sin(radians("+Area.Latitude.ToString()+")))) Asc ");
+            subQuery.Append("LIMIT 8) AS country ");
+            subQuery.Append("JOIN artists JOIN Songs ");
+            subQuery.Append("WHERE songs.artists_idArtists = artists.idArtists AND artists.Area_LocationId = country.LocationId ");
+            subQuery.Append("GROUP BY location_name order by count(location_name) desc ");
+            /*
             subQuery.Append("+ sin( radians(area.latitude) ) * sin(radians(" + area.Latitude.ToString() + "))))");
             subQuery.Append("Asc \n LIMIT 10");
             String query = "SELECT area.location_name, COUNT(song_name) FROM (" + subQuery + ") AS country JOIN artist JOIN Songs WHERE Songs.artist_id = artist.id AND artist.location_id = country.location_id GROUP BY country.location_name ORDER BY count(song)";
-            DataTable set = this.executer.ExecuteCommandWithResults(query);
+           */
+            DataTable set = this.executer.ExecuteCommandWithResults(subQuery.ToString());
             JObject j = QueryInterpreter.Instance.getQueryEntitesObject(QueryInterpreter.QueryType.AreaSongsCount, set);
             Mapper = JsonConvert.DeserializeObject<Dictionary<Area, int>>(j.ToString());
 
